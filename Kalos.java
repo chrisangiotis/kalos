8a093cc057a878dc5a7d53e0ff349f79887c7339
Chris Sangiotis <c.sangiotis@gmail.com>
2014-10-24 17:45:44 +0300

    New java class

---
+++ Kalos.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Kalos {
+	public static int getNumber(int num) {
+		return num;
+	}
+
+	public static void main(String[] args) {
+		System.out.printf("%d\n", getNumber(5));
+	}
+}
